0cf853092992e61740bb8bfb77f0224ec27e4171
realsaleemdev <saleembinyousaf006@gmail.com>
2026-03-31 15:02:47 +0500

    Add files via upload

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+export default function Header(){
+    return(
+        <>
+        <div class="container">
+    <nav class="menu">
+      <ul>
+        <li><a href="#">Home</a></li>
+        <li><a href="#">About</a></li>
+        <li><a href="#">Services</a></li>
+        <li><a href="#">Contact</a></li>
+      </ul>
+    </nav>
+  </div>
+        </>
+    );
+}
